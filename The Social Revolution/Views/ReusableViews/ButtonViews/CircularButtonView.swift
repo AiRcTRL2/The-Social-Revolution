05f1fa0ed515fd16ca532ae6d2a21cf6941cd822
Karim El Gendy <karim.elgendy@ucdconnect.ie>
2021-03-11 12:18:08 +0000

    Implement logic for custom circular button

--- The Social Revolution/Views/ReusableViews/ButtonViews/CircularButtonView.swift
+++ The Social Revolution/Views/ReusableViews/ButtonViews/CircularButtonView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct CircularButtonView: View {
+    var buttonAction: () -> ()
+    var width: CGFloat = 60
+    var height: CGFloat = 60
+    var fontSize: Double = 32.5
+    var buttonColor: Color = .blue
+    var textColor: Color = .white
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            buttonAction()
+        }, label: {
+            Text("+")
+                .font(.system(size: 32.5, weight: .ultraLight, design: .monospaced))
+                .foregroundColor(textColor)
+                .frame(minWidth: width, idealWidth: width, maxWidth: width, minHeight: height, idealHeight: height, maxHeight: height, alignment: .center)
+                .background(buttonColor)
+                .cornerRadius(width/2)
+
+        })
     }
 }
 
 struct CircularButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularButtonView()
+        CircularButtonView(buttonAction: {})
     }
 }
